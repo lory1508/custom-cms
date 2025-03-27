3bf46bacfeb0be4c4bd9018eb7d11cdbb85d6a98
Lorenzo <lorenzo.galassi91@gmail.com>
2025-03-27 09:44:29 +0000

    feat: setup tailwind

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,6 +7,13 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
   modules: ['@nuxt/content', '@nuxt/eslint', 'nuxtjs-naive-ui'],
+  css: ['~/assets/css/main.css'],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
   vite: {
     plugins: [
       AutoImport({
