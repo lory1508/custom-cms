fb8494b5bc6e07ed8bf29631fa6737f0d3005af7
Lorenzo <lorenzo.galassi91@gmail.com>
2025-03-27 09:41:37 +0000

    feat: setup naiveui

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,24 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+import AutoImport from 'unplugin-auto-import/vite'
+import { NaiveUiResolver } from 'unplugin-vue-components/resolvers'
+import Components from 'unplugin-vue-components/vite'
+
 export default defineNuxtConfig({
+  ssr: true,
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  modules: ['@nuxt/content', '@nuxt/eslint']
+  modules: ['@nuxt/content', '@nuxt/eslint', 'nuxtjs-naive-ui'],
+  vite: {
+    plugins: [
+      AutoImport({
+        imports: [
+          {
+            'naive-ui': ['useDialog', 'useMessage', 'useNotification', 'useLoadingBar'],
+          },
+        ],
+      }),
+      Components({
+        resolvers: [NaiveUiResolver()],
+      }),
+    ],
+  },
 })
